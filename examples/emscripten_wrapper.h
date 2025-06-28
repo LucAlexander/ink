a88e9819c5aea19d7874c0b1472f73316f469f21
Luc Alexander <lucskyalex@gmail.com>
2025-06-27 22:16:13 -0400

    canvas dimension functions

--- examples/emscripten_wrapper.h
+++ examples/emscripten_wrapper.h
@@ -8,3 +8,10 @@ uint8_t emscripten_start_loop(uint8_t* args){
 	emscripten_set_main_loop_arg(process_frame, args, 0, 1);
 	return 0;
 }
+EM_JS(int, canvas_get_width, (), {
+  return window.screen.width;
+});
+
+EM_JS(int, canvas_get_height, (), {
+  return window.screen.height;
+});
